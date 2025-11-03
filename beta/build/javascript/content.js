f6179188e96fbb75bbb9169830c97bb589a27e9d
Tom Chen <tc3363@drexel.edu>
2025-11-03 07:40:13 -0500

    Update image file names

---
+++ beta/build/javascript/content.js
@@ -0,0 +1,14 @@
+const locationSet = [
+    "Atlanta", 
+    "Boston", 
+    "Los Angeles", 
+    "Miami", 
+    "New York", 
+    "Phoenix", 
+    "Portland, OR", 
+    "Raleigh-Durham", 
+    "San Diego", 
+    "San Francisco", 
+    "Seattle", 
+    "Washington DC"
+]
