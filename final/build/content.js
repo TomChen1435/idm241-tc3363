b6d9733ceb19c2ab36c866d519ffb011dcdf8b3e
Tom Chen <tc3363@drexel.edu>
2025-11-15 23:54:59 -0500

    Finish JS for final build

--- final/build/content.js
+++ final/build/content.js
@@ -17,13 +17,50 @@ const locationSet = [
 const locationStringSet = [];
 
 // Elements of the DOM tree 
-const fbBanner = document.querySelector('#fb-banner');
+const fbDiv = document.querySelector('#fb');
+const fbBanner = document.querySelector('#fb-banner > img');
 const fbHoverBoard = document.querySelector('#fb-hover-board')
 const fbLocation = document.querySelector('#fb-location');
 const closeBtn = document.querySelector('#fb-close-btn');
-const fbCollection = document.querySelector('#fb-collection');
+const fbCollectionRt = document.querySelector('#fb-collection-right');
+const fbCollectionCtr = document.querySelector('#fb-collection-center');
 const fbAFLink = document.querySelector('#fb-af-link');
-const fbKLMLink = document.querySelector('#fb-klm-link');
+const fbKLLink = document.querySelector('#fb-kl-link');
+const fbPrice = document.querySelector('#fb-price');
+
+// Hover board actions
+
+// Set up a global variable for storage
+let fbHoverBoardWidths = {
+    collection: 0,
+    price: 0,
+};
+
+// Find the movable elements' widths
+function l1ElementWidths() {
+    // Get the elements' widths
+    fbHoverBoardWidths.collection = fbCollectionRt.offsetWidth + 8;
+    fbHoverBoardWidths.price = fbPrice.offsetWidth + 8;
+
+    // Set original positions
+    fbCollectionRt.style.right = `-${fbHoverBoardWidths.collection}px`;
+    fbPrice.style.left = `-${fbHoverBoardWidths.price}px`;
+};
+
+l1ElementWidths();
+
+// When resizing, update the widths
+fbHoverBoard.addEventListener('resize', l1ElementWidths);
+
+fbHoverBoard.addEventListener('mouseenter', () => {
+    fbCollectionRt.style.transform = `translateX(-${fbHoverBoardWidths.collection}px)`;
+    fbPrice.style.transform = `translateX(${fbHoverBoardWidths.price}px)`
+});
+
+fbHoverBoard.addEventListener('mouseleave', () => {
+    fbCollectionRt.style.transform = `translateX(${fbHoverBoardWidths.collection}px)`;
+    fbPrice.style.transform = `translateX(-${fbHoverBoardWidths.price}px)`
+});
 
 // Turn locations into snake case, matching the id and file name styles 
 function makeString(string) {
@@ -55,52 +92,60 @@ locationSet.forEach((location) => {
     newImage.setAttribute('alt', location);
     newImage.setAttribute('id', locationString);
     newImage.classList.add('thumbnail');
+    newImage.style.gridArea = locationString;
 
     // Add thumbnails to the carousel section 
-    fbCarousel.appendChild(newImage);
+    fbDiv.appendChild(newImage);
 });
+
+// Initially selected thumbnail
+const atlThumbnail = document.querySelector('#atlanta.thumbnail');
+atlThumbnail.classList.add('selected');
 
 // Rules for clicking the thumbnails 
 
 // Compress the snake case location string array into a single string, with the items separated by commas, for later use 
 const idSelectorSet = locationStringSet.map((locationString) => `#${locationString}`).join(', ');
 
-// Create a modifiable variable for the currently active thumbnail's sequence 
-let currentSequence = 0;
-
 // Instead of monitoring each thumbnail, monitor the parent element instead 
 
 // "event" is a parameter that contains information about the clicking action 
-fbCarousel.addEventListener('click', (event) => {
+fbDiv.addEventListener('click', (event) => {
 
     // "event.target" is what's actually clicked 
     // ".closest(selector)" walks along the DOM tree's flow and find the closest element that matches the selector, in this case, finding the closest one that's inside the selector set created earlier 
-    const cTarget = event.target.closest(idSelectorSet);
 
-    // Fetch the clicked item's ID 
-    const cTargetID = cTarget.id;
+    try {
+        const cTarget = event.target.closest(idSelectorSet);
 
-    // Find the sequence of the clicked thumbnail in the arrays and the displacement 
-    const idSequence = locationStringSet.indexOf(cTargetID);
-    const differenceSequence = idSequence - currentSequence;
-    const xShift = -6 * differenceSequence;
+        if (cTarget.classList.contains('thumbnail')) {
+            // Fetch the clicked item's ID 
+            const cTargetID = cTarget.id;
 
-    // Move the clicked thumbnail to the center 
-    fbCarousel.style.transform = `translateX(${xShift}rem)`;
+            // Find the sequence of the clicked thumbnail in the arrays 
+            const idSequence = locationStringSet.indexOf(cTargetID);
 
-    // Change the background image and update the location text at the top left corner to align with the clicked thumbnail 
+            // Remove style from the current one
+            document.querySelectorAll('.selected').forEach(element => {element.classList.remove('selected')});
 
-    // Make the elements transparent first 
-    fbBanner.style.opacity = 0;
-    fbLocation.style.opacity = 0;
+            // Apply designs to the clicked thumbnail
+            cTarget.classList.add('selected');
 
-    // Then update the elements before making them opaque again 
-    setTimeout(() => {
-        fbBanner.setAttribute('src', imgURLString(locationStringSet[idSequence], 'avif'));
-        fbLocation.innerHTML = locationSet[idSequence];
-        fbBanner.style.opacity = 1;
-        fbLocation.style.opacity = 1;
-    }, 250);
+            // Change the background image and update the location text at the top left corner to align with the clicked thumbnail 
+
+            // Make the elements transparent first 
+            fbBanner.style.opacity = 0;
+            fbLocation.style.opacity = 0;
+
+            // Then update the elements before making them opaque again 
+            setTimeout(() => {
+                fbBanner.setAttribute('src', imgURLString(locationStringSet[idSequence], 'avif'));
+                fbLocation.innerHTML = locationSet[idSequence];
+                fbBanner.style.opacity = 1;
+                fbLocation.style.opacity = 1;
+            }, 250);
+        }
+    } catch {}
 
 });
 
@@ -108,24 +153,26 @@ fbCarousel.addEventListener('click', (event) => {
 
 // Turn into a dedicated function because used repeatedly 
 function rolling(direction) {
-    fbCollection.style.opacity = 0;
-
-    setTimeout(() => {
-        if (direction === "remove") {
+    if (direction === "remove") {
+        fbCollectionCtr.style.opacity = 0;
+        setTimeout(() => {
             fbHoverBoard.classList.remove('l2');
-        } else if (direction === "add") {
+            fbCollectionRt.style.opacity = 1;
+        }, 250);
+    } else if (direction === "add") {
+        fbCollectionRt.style.opacity = 0;
+        setTimeout(() => {
             fbHoverBoard.classList.add('l2');
-        } 
-
-        fbCollection.style.opacity = 1;
-    }, 250);
+            fbCollectionCtr.style.opacity = 1;
+        }, 250);
+    }     
 }
 
 // When clicking the background image, switch to L2 
-fbCollection.addEventListener('click', () => {rolling("add")});
+fbCollectionRt.addEventListener('click', () => {rolling("add")});
 
-// When no longer hovering above the collection section or after clicking the airline links, switch back to the "hover-over-background" state 
+// When no longer hovering above the collection section or after clicking the airline links, switch back to the "L1 hover" state 
 
 closeBtn.addEventListener('click', () => {rolling("remove")});
 fbAFLink.addEventListener('click', () => {rolling("remove")});
-fbKLMLink.addEventListener('click', () => {rolling("remove")});
+fbKLLink.addEventListener('click', () => {rolling("remove")});
